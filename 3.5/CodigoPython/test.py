9e7353eb4e5ee18625214ffc8278749e049909e1
marinho112 <34494111+marinho112@users.noreply.github.com>
2021-06-29 21:45:41 -0300

    s

--- 3.5/CodigoPython/test.py
+++ 3.5/CodigoPython/test.py
@@ -23,7 +23,7 @@ def buscarInfo(fileSoup):
         for tr in table.find_all('tr'):
             ths=[]
             for th in tr.find_all('th'):
-                ths.append(th.get_text())
+                ths.append(th.get_text().strip())
             if(thFirst):
                 ths.pop(0)    
                 trs.append(["Name:",ths])
@@ -32,7 +32,7 @@ def buscarInfo(fileSoup):
             else:
                 tds=[]
                 for td in tr.find_all('td'):
-                    tds.append(td.get_text())
+                    tds.append(td.get_text().strip())
                 trs.append([ths[0],tds])
                 #j[strVar(numTable)]={ths[0]:tds}
         strVar="{"        
@@ -64,7 +64,94 @@ def buscarInfo(fileSoup):
     j=json.loads(strVar)
 
     return j
-            
+
+def EsquerdaPraDireita_CimaPraBaixo(lista,nome,header):
+    j={nome:{}}
+    #print(header)
+    for item in lista:
+        if(len(item)>0):
+            son={}
+            nomeCampo=""
+            x=0
+            y=0
+            for i in range(len(item)):
+                val=item[i]
+                if(i==0):
+                    nomeCampo=val
+                else:
+                    if(len(header[i-x])==1):
+                        son[header[i-x][0]]=val
+                    else:
+                        if(y==0):
+                            son[header[i-x][0][0]]={}
+                        son[header[i-x][0][0]][header[i-x][1][y]]=val
+                        y+=1
+                        if(y<len(header[i-x][1])):
+                            x+=1
+                        else:    
+                            y=0
+            j[nome][nomeCampo]=son
+    #print(j)        
+    return j
+
+def listaParaJson(lista,nome,header,tipo):
+    if(tipo=="EsquerdaPraDireita_CimaPraBaixo"):
+        return EsquerdaPraDireita_CimaPraBaixo(lista,nome,header)
+    print("Não reconhece tipo de tabela escolhido")
+    return {}
+
+def tabelaToJson(tabela):
+    lista=[]
+    p=-1
+    tableName=None
+    x=tabela.find("caption")
+    if(x!=None):
+        tableName=x.get_text().strip()
+    elif(str(x.attrs).count('id')!=None):
+        tableName=str(x['id'])
+    if(tableName==None):
+        print("Não foi possivel obter o nome da tabela: ")
+        print(tabela)
+        return
+    h={}
+    for tr in tabela.find_all('tr'):
+        #name=tr.get_text().strip()
+        lista.append([])
+        p+=1
+        lsTh=[]
+        for th in tr.find_all('th'):
+            value=[th.get_text().strip()]
+            if(str(th.attrs).count('colspan')!=0):
+                l=[]
+                for i in range(int(th['colspan'])):
+                    l.append(None)
+                value=[value,l]
+            lsTh.append(value)
+        if(len(lsTh)>0):
+            h[str(p)]=lsTh
+        for td in tr.find_all('td'):
+            lista[p].append(td.get_text().strip())
+            #j[name]=td.get_text().strip()
+        header=[]
+        
+    for i in h.keys():
+        if(header==[]):
+            for x in range(len(h[i])):
+               header.append(h[i][x])
+        else:
+            k=0
+            for x in range(len(h[i])):
+                item=h[i][x][0]
+                for e in range(len(header)):
+                    if(len(header[e])>1):
+                        for w in range(len(header[e][1])):
+                            if((item!=None)and(header[e][1][w]==None)):
+                                header[e][1][w]=item
+                                item=None
+
+    tipo="EsquerdaPraDireita_CimaPraBaixo"
+    return listaParaJson(lista,tableName,header,tipo)
+   
 def buscaComplemento(fileSoup):
     soup=fileSoup
     var=soup.find('body')
@@ -76,8 +163,9 @@ def buscaComplemento(fileSoup):
     var.find("div",{"class": "footer"}).replaceWith("")
     for script in var.find_all("script"):
         script.replaceWith("")
-    tabelas = soup.find_all('table')
+    tabelas = []#soup.find_all('table')
     for table in soup.find_all('table'):
+        tabelas.append(tabelaToJson(table))
         table.replaceWith("")
     
     #texto=var.get_text().split('\n')
@@ -105,11 +193,39 @@ def buscaComplemento(fileSoup):
         if(nome==""):
             nome=str(x)
         lista.append([nome,listaTexto[x]])
+    return [lista,tabelas]
     
-    return lista
-    
-    
+def verificaRelacao(key,jayKey,j):
+    if key.lower().count(jayKey.lower()) != 0:
+        return True
+    else:
+        splitType=j[key]["Size/Type:"].split('(')
+        listType=None
+        if(len(splitType)>1):
+            listType=splitType[1].replace(')','')
+            listType=listType.split(',')
+            for tpe in listType:    
+                if(jayKey.lower()==tpe.lower()):
+                    return True
+    return False  
 
+def verificaRelacaoTabela(key,jayKey,j):
+    split=key.split(',')
+    for s in split:
+        if jayKey.lower().count(s.lower().strip()) != 0:
+            return True
+    
+    splitType=j[key]["Size/Type:"].split('(')
+    listType=None
+    if(len(splitType)>1):
+        listType=splitType[1].replace(')','')
+        listType=listType.split(',')
+        for tpe in listType:    
+            if(jayKey.lower()==tpe.lower()):
+                return True
+    return False  
+
+    
 file = "elemental"
 url = 'https://www.d20srd.org/srd/monsters/'+file+'.htm'
 
@@ -119,7 +235,9 @@ html = response.read()
 
 soup = BeautifulSoup(html, 'html.parser')
 j=buscarInfo(soup)
-lista=buscaComplemento(soup)
+out=buscaComplemento(soup)
+lista=out[0]
+tabelas=out[1]
 
 #listaTamanhos=["Small","Medium","Large","Huge","Greater","Elder"]
 
@@ -159,8 +277,17 @@ jayKeys=jay.keys()
 for key in listaKeys:
     novoJ[key]["Features"]={}
     for jayKey in jayKeys:
-        if key.lower().count(jayKey.lower()) != 0 :
+        if verificaRelacao(key,jayKey,novoJ) :
             novoJ[key]["Features"][jayKey]=jay[jayKey]
+
+for key in listaKeys:
+    novoJ[key]["Tables"]={}
+    for tabela in tabelas:
+        tabKeys=tabela.keys()
+        for tabKey in tabKeys:
+            if verificaRelacaoTabela(key,tabKey,novoJ) :
+                novoJ[key]["Tables"][tabKey]=tabela[tabKey]
+
 #print(jay)
 #arquivo = open('Monstros/'+file+'.json', 'w')
 arquivo = open('Monstros/'+file+'aa.json', 'w')
